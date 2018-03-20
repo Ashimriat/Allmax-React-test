3688e5d0ed585e6313c9934e3ecbe3c733523666
Ashi <ashimriat42@gmail.com>
2018-03-20 13:33:04 +0700

    Fix

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import TaskEditForm from '../TaskEditForm/TaskEditForm';
 import './Task.css';
 
-class Task extends Component {
+class Task extends PureComponent {
 
     constructor(props) {
 
